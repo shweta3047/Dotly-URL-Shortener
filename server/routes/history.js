e2f5da3d72e2caf2a938447c886959e24a10582d
Shweta <shweta3047@gmail.com>
2020-07-29 16:23:50 +0530

    deployed version

--- server/routes/history.js
+++ server/routes/history.js
@@ -2,9 +2,10 @@ const express=require('express');
 const router=express.Router();
 const User=require('../models/user');
 const mongoose=require('mongoose');
-const requireAuth=require('../middleware/auth');
+const authRequired=require('../middleware/auth');
 
-router.get("/history",requireAuth,(req,res)=>{
+
+router.get("/history",authRequired,(req,res)=>{
     User.findById(req.user._id).populate({path:'urls',populate:{path:'url'}}).select("-password")
     .then(savedUrls=>{
         console.log(savedUrls)
