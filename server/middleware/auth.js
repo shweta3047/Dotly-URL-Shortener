3c53523085d8ee3740cfa360ed8118f0ff0c59c7
Shweta <shweta3047@gmail.com>
2020-07-29 15:36:36 +0530

    deployed version

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,7 +11,7 @@ module.exports=(req,res,next)=>{
     const token=authorization.replace('Bearer ',"")
     jwt.verify(token,JWT_SECRET,(err,payload)=>{
         if(err)
-        return res.status(401).json({error:"User must  be logged in"});
+        return res.status(401).json({error:"User must  be logged in got it"});
         const {_id}=payload;
         User.findById(_id).then(userData=>{
             req.user=userData;
