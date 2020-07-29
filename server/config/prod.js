2674d202b5c9091ea05d065af195c87ecd30cde0
Shweta <shweta3047@gmail.com>
2020-07-29 12:21:17 +0530

    deployed version

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,5 @@
-const { baseURL } = require("./dev");
-
 module.exports={
     MONGOURI:process.env.MONGOURI,
     JWT_SERCRET:process.env.JWT_SERCRET,
-    baseURL:process.env.baseURL
+    baseURL:process.env.BASEURL
 }
